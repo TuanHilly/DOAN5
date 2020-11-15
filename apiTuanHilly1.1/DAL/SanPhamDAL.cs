758be13621b1e95e2c3c9d8e26a5ac61ae31c52f
TuanHilly <70497399+TuanHilly@users.noreply.github.com>
2020-11-15 11:14:37 +0700

    submit

--- apiTuanHilly1.1/DAL/SanPhamDAL.cs
+++ apiTuanHilly1.1/DAL/SanPhamDAL.cs
@@ -77,8 +77,9 @@ namespace DAL
                 "@mota", model.mota,
                 "@dongia", model.dongia,
                 "@soluong", model.soluong,
-                "@anh", model.anh,
-                "@status", model.status);
+                "@anh", model.anh
+                //"@status", model.status,
+                );
                 if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
                 {
                     throw new Exception(Convert.ToString(result) + msgError);
